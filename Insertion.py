4aca922afdd9cee56fa5be010bda5bad4e6ea76e
Gavinilalithasri <112418072+Gavinilalithasri@users.noreply.github.com>
2022-10-07 20:50:26 +0530

    Add files via upload

---
+++ Insertion.py
@@ -0,0 +1,27 @@
+def Insertion(a):
+     
+   for i in range(1,len(a)):
+        
+         j=i-1
+        
+         nxtele=a[i]
+        
+         while(a[j]>nxtele and j>=0):
+            
+              a[j+1]=a[j]
+            
+              j=j-1
+        
+         a[j+1]=nxtele
+    
+   print(a)  
+
+n=int(input("enter size of array:")) 
+
+arr=[]
+for i in range(n):
+    
+    ele=int(input("enter element:"))
+    
+    arr.append(ele)
+Insertion(arr)
